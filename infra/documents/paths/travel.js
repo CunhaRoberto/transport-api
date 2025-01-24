63ab4024e6554c3e901b6d896eb4d297bbb6db51
Roberto Cunha <rcunha@live.com>
2025-01-24 18:20:01 -0300

    feat: inciado fluxo create travel

--- infra/documents/paths/travel.js
+++ infra/documents/paths/travel.js
@@ -22,6 +22,65 @@ export default
         }
       }
       },
+      post: {
+        summary: 'Create travel',
+        tags: ['Travel'],
+        description: 'Create travel',
+        parameters: [
+          {
+            in: 'body',
+            name: 'body',
+            description: 'description',
+            require: 'require',
+            schema: {
+              type: 'object',
+              properties: {
+                idBus: {
+                  type: 'string',
+                  format: 'uuid',
+                  require: true,
+                  example: '95af06d5-56c5-435a-a8e9-8fe1201c3721'
+                },
+                idRoute: {
+                  type: 'string',
+                  format: 'uuid',
+                  require: true,
+                  example: '95af06d5-56c5-435a-a8e9-8fe1201c3721'
+                },
+                dateIntit: {
+                  type: 'string',
+                  format: 'date-time',
+                  require: true,
+                  example: '2025-08-15T20:27:42.769Z'
+                },
+                dateFinish: {
+                  type: 'string',
+                  format: 'date-time',
+                  require: true,
+                  example: '2025-08-16T20:27:42.769Z'
+                }                
+              }
+            }
+          }
+        ],
+        responses: {
+          201: {
+            description: 'Created successfully'
+          },
+          400: {
+            description: 'Bad request'
+          },
+          404: {
+            description: 'Not found'
+          },
+          409: {
+            description: 'Conflict'
+          },
+          500: {
+            description: 'Internal Server Error'
+          }
+        }
+        },
     },
     '/routes/': {
       get: {
--- src/transport/controllers/TravelController.mjs
+++ src/transport/controllers/TravelController.mjs
@@ -4,8 +4,7 @@ import { default as Presenter } from '../presenters/SearchBus.mjs'
 import TravelRepository from '../repositories/Travel.mjs'
 import RepositoryImpl from '../../../infra/repository/index.mjs'
 import Search from '../useCases/SearchTravel.mjs'
-//import { default as CreateUsersIdValidator } from '../validators/CreateUsers.mjs'
-import bcrypt from 'bcrypt';
+import { default as CreateTravelValidator } from '../validators/CreateUsers.mjs'
 
 const Repository = new TravelRepository(RepositoryImpl)
 
@@ -22,14 +21,13 @@ export async function search(request, response, next) {
 
 export async function create(request, response, next) {
   try {
-    const userDto = request.body
-    await CreateUsersIdValidator.validate(userDto)
+    const travelDto = request.body
+    await CreateTravelValidator.validate(travelDto)    
     
-    userDto.password = await bcrypt.hash(userDto.password, 10);
-    const createUsersIdUseCase = new CreateUserId(Repository)
-    const resultUsers = await createUsersIdUseCase.execute(userDto)
-    const presentUsers = await BusPresenter.present(resultUsers)
-    return response.status(201).json(presentUsers)
+    const createUseCase = new Create(Repository)
+    const result = await createUseCase.execute(travelDto)
+    const present = await Presenter.present(result)
+    return response.status(201).json(present)
   } catch (error) {
     return next(error)
   }
