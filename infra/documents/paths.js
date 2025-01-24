b7abe1210fff00089ce95334f589a77d8f55dd97
cunhaRoberto <rcunha@live.comm>
2025-01-23 21:35:09 -0300

    feat: add travel em swagger

--- infra/documents/paths.js
+++ infra/documents/paths.js
@@ -1,14 +1,16 @@
 import healthCheck from './paths/healthCheck.js'
 import user from './paths/user.js'
 import bus from './paths/bus.js'
+import travel from './paths/travel.js'
 import authentication from './paths/authentication.js'
 
 
 let paths = Object.assign(
   healthCheck,
   user,
+  authentication,
   bus,
-  authentication
+  travel
 )
 
 export default paths
