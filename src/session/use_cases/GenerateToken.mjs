67bc1c14b1b1537ee671f6828f8e206bb1ef11e3
cunhaRoberto <rcunha@live.comm>
2025-05-11 17:33:19 -0300

    fix: ajuste retorno token

--- src/session/use_cases/GenerateToken.mjs
+++ src/session/use_cases/GenerateToken.mjs
@@ -3,9 +3,7 @@ import jwt from 'jsonwebtoken'
 import auth from '../../../config/auth.mjs'
 import ms from 'ms'
 
-
 const generateToken = (idUser, cpfUser) => { 
-
   const {
     secret_token,        
     secret_refresh_token,
@@ -13,34 +11,35 @@ const generateToken = (idUser, cpfUser) => {
     expires_in_refresh_token
   } = auth
 
-  const expiresToken =  Date.now() + ms(expires_in_token) 
   const token = jwt.sign(
     {
       idUser,
-      cpf: cpfUser,
-      expiresIn : expiresToken,          
+      cpf: cpfUser
     },
     secret_token,
+    {
+      expiresIn: expires_in_token
+    }
   ) 
   
-  const expiresRefreshToken =  Date.now() + ms(expires_in_refresh_token)
   const refresh_token = jwt.sign(
     {
       idUser,
-      cpf: cpfUser,
-      expiresIn : expiresRefreshToken
+      cpf: cpfUser
     },
     secret_refresh_token,
+    {
+      expiresIn: expires_in_refresh_token
+    }
   )      
         
   return {
     token, 
     refresh_token,
-    expiresTokenDate: new Date(expiresToken)  
+    expiresTokenDate: new Date(Date.now() + ms(expires_in_token))  
   } 
 }
 
 export default {
   generateToken
 }
-  
